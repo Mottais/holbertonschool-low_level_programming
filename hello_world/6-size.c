651c86647d3fd37c125bf35965f78278f44b42aa
Mottais <mottais.afy@outlook.fr>
2023-10-13 03:33:22 -0700

    Task 9.6

---
+++ hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long int long_intType;
+	long long int long_long_intType;
+	float floatType;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long_intType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long_long_intType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	return (0);
+}

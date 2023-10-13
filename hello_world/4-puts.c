06073944ff66a635d82359a738991ea29d17ab31
Mottais <mottais.afy@outlook.fr>
2023-10-13 01:03:19 -0700

    Task 9.4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* prints message lowed by a new line. */
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}

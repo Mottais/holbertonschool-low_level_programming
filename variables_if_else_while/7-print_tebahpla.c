31e528f37d8af13046b32e6a6a797ebce7a8db4b
Mottais <mottais.afy@outlook.fr>
2023-10-16 05:34:15 -0700

    Task 10.7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+
+{
+	/* your code goes there */
+	int i;
+
+	for (i = 122; i <= 97 ; i--)
+	{
+		putchar (i);
+	}
+	putchar (10);
+	return (0);
+}

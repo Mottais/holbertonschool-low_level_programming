9c9ea518396ef0be2e6250d0d07f65d90006b8e3
Mottais <mottais.afy@outlook.fr>
2023-10-16 05:43:04 -0700

    Task 10.8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+
+{
+	/* your code goes there */
+	int i;
+
+	for (i = 48; i <= 57 ; i++)
+	{
+		putchar (i);
+	}
+	for (i = 97; i <= 102 ; i++)
+	{
+		putchar (i);
+	}
+	putchar (10);
+	return (0);
+}

4f699d1f74c6c4192700e3f864cdf1f8f36f3602
Mottais <mottais.afy@outlook.fr>
2023-10-16 07:03:28 -0700

    Task 11.0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+
+{
+	/* your code goes there */
+	putchar (95);
+	putchar (112);
+	putchar (117);
+	putchar (116);
+	putchar (99);
+	putchar (104);
+	putchar (97);
+	putchar (114);
+	putchar (10);
+	return (0);
+}

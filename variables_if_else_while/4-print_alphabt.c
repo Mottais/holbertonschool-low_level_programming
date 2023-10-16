d449648fb860942d574e22eb2c24c51a1400f87b
Mottais <mottais.afy@outlook.fr>
2023-10-16 05:12:50 -0700

    Task 10.4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	/* your code goes there */
+	int i;
+
+	for (i = 97; i <= 122 ; i++)
+	{
+		if ((i != 101) && (i != 112))
+		{
+		putchar (i);
+		}
+	}
+	putchar (10);
+	return (0);
+}

7d41cfd2843490cc178b4de3c7ec575fcc268840
Mottais <mottais.afy@outlook.fr>
2023-10-17 01:57:20 -0700

    Task 11.3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints ten times the alphabet, in lowercase.
+ * _islower - prints ten times the alphabet, in lowercase.
  * @c: input character
- *
- * Description: function that if c is lowercase character.
  *
  * Return: 1 if c is lowercase 0 otherwise
 */
+
 int _islower(int c)
 {
 	/* your code goes there */
@@ -16,8 +15,7 @@ int _islower(int c)
 		return (1);
 	}
 	else
-	{	
+	{
 		return (0);
 	}
 }
-

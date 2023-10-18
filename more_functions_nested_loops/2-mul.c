a4297af2213552f46ed3dc21afb6fcfe5fe6a23a
Mottais <mottais.afy@outlook.fr>
2023-10-18 13:16:13 -0700

    Task 13

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9).
+ * @a: input number
+ * @b: input number
+ *
+ * Return: a x b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

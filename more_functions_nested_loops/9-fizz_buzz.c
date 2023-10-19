641b7c683ce7467b7a42a81b0518ab3b230ef11f
Mottais <mottais.afy@outlook.fr>
2023-10-19 13:09:32 -0700

    Task 13.9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,19 +11,18 @@ int main(void)
 
 		for (i = 1; i <= 100 ; i++)
 		{
-/*
+
 			if (!(i % 3))
 			printf("Fizz");
 
 			if (!(i % 5))
 			printf("Buzz");
-*/
+
 			if ((i % 3) && (i % 5))
 			printf("%d", i);
 
-/*			if (i != 100)
- *			printf("-");
- */
+			if (i != 100)
+			printf(" ");
 		}
 
 	printf("\n");

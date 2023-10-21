4170e0038f1216bafb94abf04383f3b5e044f6ae
Mottais <mottais.afy@outlook.fr>
2023-10-21 03:21:58 -0700

    Task 13.11

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long nombre = 612852475143;
+	int i;
+
+	for (i = 2; i <= nombre; i++)
+	{
+		if ((nombre % i) == 0)
+			nombre = nombre / i;
+	}
+	return (i);
+}

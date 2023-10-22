0d7fcec6fc4ec83243a01ea403262f2440d5d334
Mottais <mottais.afy@outlook.fr>
2023-10-22 03:52:39 -0700

    Task 13.12

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -3,34 +3,41 @@
 
 /**
  * print_number - print_numberd
- * @: input number
+ * @n: input number
  */
 
 void print_number(int n)
 {
-	long p = 1;
-	long ln = n;
+	int p = 1;
 
-	if (ln == 0)
+	if (n == 0)
 	{
 		_putchar('0');
 	}
 	else
 	{
-		if (ln < 0)
+		if (n < 0)
 		{
-			ln = ln * (-1);
 			_putchar('-');
+			if (n == -2147483648)
+			{
+				_putchar('2');
+				n = n + 2000000000;
+			}
+			n = n * (-1);
 		}
 
-		while ((ln / p) != 0)
+		while ((n / p) != 0 && p < 1000000000)
 		{
 			p = p * 10;
 		}
 
+		if (n / 1000000000 > 0)
+		_putchar('0' + (n / 1000000000));
+
 		while (p != 1)
 		{
-			_putchar('0' + (ln / (p / 10)) - 10 * (ln / p));
+			_putchar('0' + (n / (p / 10)) - 10 * (n / p));
 			p = p / 10;
 		}
 	}

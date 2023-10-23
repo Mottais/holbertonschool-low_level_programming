ace4e9c9449f8a253f91549d962ee511cd32dc0f
Mottais <mottais.afy@outlook.fr>
2023-10-23 12:11:35 -0700

    Task 14.5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: input string
+ *
+ */
+
+void rev_string(char *s)
+{
+	int len;
+	int i;
+	char temp;
+
+	len = 0;
+	while (*(s + len) != 0)
+	{
+		len++;
+	}
+
+	for (i = 0; i < ((len / 2) + 1); i++)
+	{
+		temp = *(s + (len - i - 1));
+		*(s + (len - i - 1)) = *(s + i);
+		*(s + i) = temp;
+	}
+	*(s + len) = 0;
+}

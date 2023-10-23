1f6266e77a885668bc0b0a0c98d5730050fe1ec5
Mottais <mottais.afy@outlook.fr>
2023-10-23 05:18:04 -0700

    Task 14.0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset_to_98
+ * @n: input pointer integer
+ *
+ * Return: x + y
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/modele.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * xxxxx - comment....
+ * @x: input integer
+ * @y: input integer
+ *
+ * Return: x + y
+ */
+
+int xxxxx(int x, int y)
+{
+	return (0);
+}

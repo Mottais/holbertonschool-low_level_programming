34371397a24314ef037da23914241e557463dd9d
Mottais <mottais.afy@outlook.fr>
2023-10-23 13:35:14 -0700

    Task 14.8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -10,12 +10,13 @@
 void print_array(int *a, int n)
 {
 	int i;
+
 	if (n > 0)
 	{
 		printf("%d", *(a + 0));
-		for (i = 1; i < n ; i++)
+		for (i = 1; i < n; i++)
 		{
-			printf(", %d", *(a + i));
+		printf(", %d", *(a + i));
 		}
 	}
 	printf("\n");

058e30c7863f27fda102e9efeedc8271e6532a72
Mottais <mottais.afy@outlook.fr>
2023-10-23 12:46:36 -0700

    Task 14.7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: input string
+ *
+ */
+
+void puts_half(char *str)
+{
+	int len;
+	int i;
+
+	len = 0;
+
+	while (*(str + len) != 0)
+	{
+		len++;
+	}
+
+	for (i = len / 2; i < len ; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar(10);
+}

3ebab60629ed011a874c8e596f386318bc58a923
Mottais <mottais.afy@outlook.fr>
2023-10-24 10:56:02 -0700

    Task 14.2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy -  copies a string.
+ * @dest: input string destibation
+ * @src : input string source
+ * @n : nubre caraters
+ *
+ * Return: string concatenates
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *dest_start = dest;
+	int i, j;
+
+	i = 0;
+
+	while (*(src + i) != '\0')
+	{
+	i++;
+	}
+printf("long src : %d\n", i);
+printf("n : %d\n", n);
+	j = 0;
+	while (j < i && j < n)
+	{
+		*(dest_start + j) = *(src + j);
+		j++;
+	}
+
+	while ((*(dest + j) != '\0') && j < n)
+	{
+		*(dest_start + j) = 0;
+		j++;
+	}
+
+	return (dest_start);
+}

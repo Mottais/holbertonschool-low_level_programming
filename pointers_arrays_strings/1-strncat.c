23dcca3bffaf6a2d2fd78818d31c60a5c86c5baa
Mottais <mottais.afy@outlook.fr>
2023-10-24 08:53:09 -0700

    Task 14.1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncpy - concatenates two strings n caraters
+ * @dest: input string destibation
+ * @src : input string source
+ * @n : nubre caraters
+ *
+ * Return: string concatenates
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *dest_start = dest;
+	int i, j;
+
+	i = 0;
+
+	while (*(dest + i) != '\0')
+	{
+	i++;
+	}
+
+	j = 0;
+	while ((*(src + j) != '\0') && j < n)
+	{
+		*(dest_start + i) = *(src + j);
+		i++;
+		j++;
+	}
+
+	return (dest_start);
+}

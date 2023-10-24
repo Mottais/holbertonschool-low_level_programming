fbafe2eeeb2b9c277cbce9186863a935cf62efcc
Mottais <mottais.afy@outlook.fr>
2023-10-24 13:06:28 -0700

    Task 15.3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strcmp - compares two strings..
+ * @s1: input string
+ * @s2: input string
+ *
+ * Return: exactly like strcmp
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int lens1 = strlen(s1);
+	int lens2 = strlen(s2);
+	int n, i;
+
+	if (lens1 < lens2)
+	n = lens1;
+	else
+	n = lens2;
+	i = 0;
+	while (s1[i] == s2[i] && i < n)
+	i++;
+
+	return (s1[i] - s2[i]);
+}

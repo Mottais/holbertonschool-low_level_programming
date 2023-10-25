060017c6a4aad90c2aa5fcdb2b8cbec13b15dcbb
Mottais <mottais.afy@outlook.fr>
2023-10-25 10:40:51 -0700

    Task 15.4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -12,7 +12,7 @@ void reverse_array(int *a, int n)
 	int i;
 	int temp;
 
-	for (i = 0; i <= (n / 2); i++)
+	for (i = 0; i <= ((n / 2) - (n % 2)); i++)
 	{
 		temp = a[n - 1 - i];
 		a[n - 1 - i] = a[i];

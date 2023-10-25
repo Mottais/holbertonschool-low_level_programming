da8d7197de78c368878cf0e9d05c6408fe78fd12
Mottais <mottais.afy@outlook.fr>
2023-10-25 02:13:05 -0700

    Task 15.4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array - everses the content of an array of integers
+ * @a: input array
+ * @n: input integer
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp;
+
+	for (i = 0; i <= (n / 2); i++)
+	{
+		temp = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = temp;
+	}
+
+}

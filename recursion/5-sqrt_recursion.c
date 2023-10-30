83228ce10a712dcb2d5e917ffac26f3c678742b2
Mottais <mottais.afy@outlook.fr>
2023-10-30 15:02:54 -0700

    Task 17.5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: input integer
+ *
+ * Return: returns the natural square root of n
+ */
+
+
+/**
+ * carre_de_i - cherche si i au carré egale n
+ * @i : integer incrémenté
+ * @n : le carré recherché
+ *
+ * Teturn : i si trouvé sinon -1
+ */
+
+int carre_de_i(int i, int n)
+{
+		if (i * i == n)
+					return (i);
+			if (i * i > n)
+						return (-1);
+				return (carre_de_i(i + 1, n));
+}
+
+int _sqrt_recursion(int n)
+{
+		return (carre_de_i(0, n));
+}

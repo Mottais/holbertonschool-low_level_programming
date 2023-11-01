ed89c73d6991cf16d32eddde13136fdf8b0878ca
Mottais <mottais.afy@outlook.fr>
2023-11-01 01:54:11 -0700

    Task 19.0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,10 +2,12 @@
 
 /**
  * main - prints name program
+ * @argc: unused
+ * @argv: array argument
  *
  * Return: allway 0
  */
-int main(int argc __attribute__((unused)), char **argv)
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);

780eaa0249d0645a7a31f42435b19e7eba5087db
Mottais <mottais.afy@outlook.fr>
2023-11-06 03:52:44 -0800

    Task 20.0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,14 +15,14 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return(0);
-	
+		return (0);
+
 	str = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 		str[i] = c;
 
-	if (str = 0)
-		return(0);
-	
-	return(str);
+	if (str == 0)
+		return (0);
+
+	return (str);
 }

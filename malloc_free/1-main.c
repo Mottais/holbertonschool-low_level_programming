864478604d8b4e6b3d76384950fb4d1c70d879d5
Mottais <mottais.afy@outlook.fr>
2023-11-06 07:40:33 -0800

    Task 20.1

--- malloc_free/1-main.c
+++ malloc_free/1-main.c
@@ -9,15 +9,15 @@
  *     */
 int main(void)
 {
-	    char *s;
+	char *s = "ABCDE";
 
-	        s = _strdup("Duplicated");
-		    if (s == NULL)
-			        {
-					        printf("failed to allocate memory\n");
-						        return (1);
-							    }
-		        printf("%s\n", s);
-			    free(s);
-			        return (0);
+	s = _strdup(s);
+	if (s == NULL)
+	{
+	printf("failed to allocate memory\n");
+	return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	return (0);
 }

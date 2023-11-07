20436d10942dda29c862aabe7c4c81de81fae5d6
Mottais <mottais.afy@outlook.fr>
2023-11-07 12:38:31 -0800

    Task 21.1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len_s2++;
 
 	if (n < len_s2)
-		len_s2 = n;
+		len_s2 = n - 1;
 
 	len_s1_ns2 = len_s1 + len_s2 + 1;
 	s1_ns2 = malloc(len_s1_ns2);

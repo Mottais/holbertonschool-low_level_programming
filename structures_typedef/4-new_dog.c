707a087a94aea3f20bd349d258bfb4ccd59b16bd
Mottais <mottais.afy@outlook.fr>
2023-11-13 15:42:48 -0800

    Task 23.4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,14 +14,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 
 		new_dog = malloc(sizeof(dog_t));
-
-		if (new_dog == NULL)
-			return (NULL);
-
 		new_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
 		new_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 
-		if (new_dog->owner == NULL || new_dog->name == NULL)
+		if (new_dog->owner == NULL || new_dog->name == NULL || new_dog == NULL)
 		{
 			free(new_dog->name);
 			free(new_dog->owner);

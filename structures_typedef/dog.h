668e1ddb36de2a904f7466fd7c004842a915fc29
Mottais <mottais.afy@outlook.fr>
2023-11-13 03:42:25 -0800

    Task 22.3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,14 +1,18 @@
+#ifndef _EXISTE_DEJA_
+#define _EXISTE_DEJA_
+
 /**
- * dog - dog data
+ * struct dog - dog data
  * @name: name dog
  * @age: age dog
  * @owner: owner dog
- *
  * Description: dog data
  */
-
-struct dog {
-	char *name,
+struct dog
+{
+	char *name;
 	float age;
 	char *owner;
 };
+
+#endif

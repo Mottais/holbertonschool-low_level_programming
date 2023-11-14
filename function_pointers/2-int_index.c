ce79ba0b72d6a72a15ee71e130866a5a75d15684
Mottais <mottais.afy@outlook.fr>
2023-11-14 06:11:38 -0800

    Task 23.2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: input aurray
+ * @size: input size of the array
+ * @cmp: pointer to function
+ * Return: index and -1 if fail or false
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}

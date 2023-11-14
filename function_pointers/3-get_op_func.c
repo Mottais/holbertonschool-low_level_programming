aa451c2885dc21b810664abe8f9d88c92bc3a431
Mottais <mottais.afy@outlook.fr>
2023-11-14 12:37:05 -0800

    Task 24.3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -8,16 +8,15 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	int i;
-
-	op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
-	};
+		op_t ops[] = {
+			{"+", op_add},
+			{"-", op_sub},
+			{"*", op_mul},
+			{"/", op_div},
+			{"%", op_mod},
+			{NULL, NULL}
+		};
+	int i = 0;
 
 	for (i = 0; i < 5; i++)
 	{

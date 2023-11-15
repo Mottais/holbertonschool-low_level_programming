a1430f3925163901306b4d5e0ae2d3a27c7879a0
Mottais <mottais.afy@outlook.fr>
2023-11-15 12:01:51 -0800

    Task 25.3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,4 +5,18 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * CodeFormat_Fonction - CodeFormat-->NameFonction
+ *
+ * @Code_Format: The code of format
+ * @Pointeur_Fonction_Print: The function associated
+ */
+typedef struct CodeFormat_Fonction
+{
+	char *Code_Format;
+	void (*Pointeur_Fonction_Print)();
+} TypeDef_CodeFormat_Fonction;
+
+void print_all(const char * const format, ...);
+
 #endif

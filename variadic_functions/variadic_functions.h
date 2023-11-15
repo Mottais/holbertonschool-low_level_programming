f48a8a399a17a314649bcefea5121c242d38ff82
Mottais <mottais.afy@outlook.fr>
2023-11-15 00:15:47 -0800

    Task 25.0

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef _EXISTE_DEJA_
+#define _EXISTE_DEJA_
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif

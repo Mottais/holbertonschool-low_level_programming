cdc7b0052c987d9fd95de4984f871ed3c7e06dd7
Mottais <mottais.afy@outlook.fr>
2023-11-29 13:32:26 -0800

    Task 2

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * get_bit - returns the value of a bit at a given index.
+ * @n: the valuei to get a bit 
+ * @index: index of the bit
+ *
+ * Return: returns the value of a bit at a given index, return -1 if failure
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned masque = 1;
+
+	if (index >= sizeof(long int) * 8)
+		return (-1);
+
+	masque = masque << index;
+
+	return ((n & masque) > 0);
+}

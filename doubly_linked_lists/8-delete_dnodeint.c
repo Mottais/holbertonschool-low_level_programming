e2cd53513a17f4073dd37b6ffc5692ffaf377967
Mottais <mottais.afy@outlook.fr>
2023-11-29 05:29:28 -0800

    Task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -11,8 +11,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *current = *head;
 	unsigned int i = 0;
 
-	/* if (head == NULL || *head == NULL) */
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 	while (current != NULL && i  < index)
 	{

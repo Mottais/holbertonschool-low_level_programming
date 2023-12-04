930769c4381ebe3bb4827f135dc6e4460d14720e
Mottais <mottais.afy@outlook.fr>
2023-12-04 08:40:00 -0800

    Task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * read_textfile - Create a function that creates a file.
+ *
+ * @filename: name of file
+ * @text_content: the text to print on file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len_text;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		return (-1);
+
+	len_text = strlen(text_content);
+
+	fd = open(filename, O_CREAT | O_WRONLY , 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (write(fd, text_content, len_text) == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}

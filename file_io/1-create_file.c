f1ca091b90c340cd01fbc3371f06373b8e65a10f
Mottais <mottais.afy@outlook.fr>
2023-12-04 08:43:14 -0800

    Task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * read_textfile - Create a function that creates a file.
+ * create_file - Create a function that creates a file.
  *
  * @filename: name of file
  * @text_content: the text to print on file
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 
 	len_text = strlen(text_content);
 
-	fd = open(filename, O_CREAT | O_WRONLY , 0600);
+	fd = open(filename, O_CREAT | O_RDWR, 0600);
 	if (fd == -1)
 		return (-1);
 

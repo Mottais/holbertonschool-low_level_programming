da94e48736de9f1ec2de58b75b665bd739c9d5ac
Mottais <mottais.afy@outlook.fr>
2023-12-04 08:45:53 -0800

    Task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 
 	len_text = strlen(text_content);
 
-	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_RDWR, 0600);
 	if (fd == -1)
 		return (-1);
 

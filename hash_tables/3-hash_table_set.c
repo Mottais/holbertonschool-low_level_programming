9d888638c04ffa2bc7d494360ff5917a3392a65e
Mottais <mottais.afy@outlook.fr>
2023-12-06 05:02:51 -0800

    Task 6

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,51 +1,48 @@
 #include "hash_tables.h"
 /**
- * hash_table_set - adds an element to the hash table.
- * @ht: the pointer to the hash table
- * @key: key of element to add
- * @value: value of element to add
+ * hash_table_set - add to the hash table
  *
- * Return: 1 (success) or 0 (failure)
+ * @ht: hash table to add
+ * @key: is the key
+ * @value: is the value of key
+ * Return: 1 if success
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int size, index;
-	hash_node_t *ptr, *new_node;
+	unsigned long int index = 0;
+	hash_node_t *temp = NULL;
+	hash_node_t *new_node;
 
-	if (ht == NULL || key == NULL)
+	if (!ht)
 		return (0);
+
 	if (value == NULL)
 		value = "";
-	size = ht->size;
-	index = key_index((unsigned char *)key, size);
-	ptr = ht->array[index];
 
-	/* Gestion collision (clé déjà présente pour cet index) */
-	while (ptr != NULL)   /* boucle sur la liste de l'index */
+	index = key_index((unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
+	while (temp)
 	{
-		if (strcmp(ptr->key, key) == 0) /* si clé existe déjà */
+		if (!strcmp(temp->key, key))
 		{
-			ptr->value = strdup(value); /* mise à jour valeur */
+			free(temp->value);
+			temp->value = strdup(value);
 			return (1);
 		}
-		ptr = ptr->next;
+		temp = temp->next;
 	}
 
-	/* Création nouveau noeud */
 	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	if (!new_node)
 		return (0);
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
 
-	if (new_node->key == NULL || new_node->value == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
+	new_node->key = strdup((char *)key);
 
-	/* Ajout nouveau noeud au début de la liste */
+	new_node->value = strdup((char *)value);
+
 	new_node->next = ht->array[index];
+
 	ht->array[index] = new_node;
 
 	return (1);

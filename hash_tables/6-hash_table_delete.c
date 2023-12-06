352a72fcff2b733d22f6d12da70b51937e6d47e6
Mottais <mottais.afy@outlook.fr>
2023-12-06 04:58:35 -0800

    Task 6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,30 +1,31 @@
 #include "hash_tables.h"
 /**
- * hash_table_delete - free the hash table
- * @ht: the pointer to the hash table
+ * hash_table_delete - delete a hash table
  *
+ * @ht: hash table
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	hash_node_t *temp = NULL;
+	unsigned long int i = 0;
 
-	unsigned long int size, i;
-	hash_node_t *ptr, *next_noeud;
-
-	if (ht == NULL)
+	if (!ht)
 		return;
 
-	size = ht->size;
-	for (i = 0; i < size; i++)
+	while (i < ht->size)
 	{
-		ptr = ht->array[i];
-		while (ptr != NULL)
+		while (ht->array[i])
 		{
-			next_noeud = ptr->next;
-			free(ptr->key);
-			free(ptr->value);
-			free(ptr);
-			ptr = next_noeud;
+			temp = ht->array[i];
+			ht->array[i] = ht->array[i]->next;
+			if (temp->key)
+				free(temp->key);
+			if (temp->value)
+				free(temp->value);
+			free(temp);
 		}
+		free(ht->array[i]);
+		i++;
 	}
 	free(ht->array);
 	free(ht);
